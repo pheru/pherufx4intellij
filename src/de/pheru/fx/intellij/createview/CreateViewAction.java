b3cec8d08f0ebb65af21f846f49cefd558ccbcab
pheru <philipp.bruckner@freenet.de>
2015-11-13 14:44:05 +0100

    #3, #7

--- src/de/pheru/fx/intellij/createview/CreateViewAction.java
+++ src/de/pheru/fx/intellij/createview/CreateViewAction.java
@@ -3,20 +3,21 @@ package de.pheru.fx.intellij.createview;
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.ide.fileTemplates.FileTemplateUtil;
+import com.intellij.ide.projectView.ProjectView;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.JavaDirectoryService;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.PsiNameHelper;
 import com.intellij.psi.PsiPackage;
-import com.intellij.psi.impl.file.PsiDirectoryFactory;
 
 import java.util.Properties;
 
@@ -24,6 +25,11 @@ import java.util.Properties;
  * Created by Philipp on 10.11.2015.
  */
 public class CreateViewAction extends AnAction {
+
+    //                NotificationGroup GROUP_DISPLAY_ID_INFO = new NotificationGroup("Group", NotificationDisplayType.BALLOON, true);
+//                Notification notification = GROUP_DISPLAY_ID_INFO.createNotification("Title", "Test",
+//                        NotificationType.INFORMATION, null);
+//                Notifications.Bus.notify(notification, e.getProject());
 
     @Override
     public void update(AnActionEvent e) {
@@ -49,39 +55,98 @@ public class CreateViewAction extends AnAction {
     }
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        CreateViewDialog dialog = new CreateViewDialog(e.getProject());
+    public void actionPerformed(AnActionEvent event) {
+        PsiDirectory targetDirectory = PsiManager.getInstance(event.getProject()).findDirectory(DataKeys.VIRTUAL_FILE.getData(event.getDataContext()));
+        VirtualFile sourceRootFile = ProjectFileIndex.SERVICE.getInstance(event.getProject()).getSourceRootForFile(DataKeys.VIRTUAL_FILE.getData(event.getDataContext()));
+        PsiDirectory sourceRootDirectory = PsiManager.getInstance(event.getProject()).findDirectory(sourceRootFile);
+
+        String qualifiedPackageForTargetDirectory = getQualifiedPackageForTargetDirectory(sourceRootDirectory, targetDirectory);
+        CreateViewDialog dialog = new CreateViewDialog(event.getProject(), qualifiedPackageForTargetDirectory.isEmpty() ? "" : qualifiedPackageForTargetDirectory + ".");
         dialog.show();
         if (dialog.getExitCode() != 0) {
             return;
         }
+        String input = dialog.getInput();
+        String viewName;
+        String qualifiedPackageName;
+        if (input.contains(".")) {
+            viewName = input.substring(input.lastIndexOf('.') + 1);
+            qualifiedPackageName = input.substring(0, input.lastIndexOf('.'));
+        } else {
+            viewName = input;
+            qualifiedPackageName = "";
+        }
+
         Properties properties = FileTemplateManager.getDefaultInstance().getDefaultProperties();
-        properties.setProperty("VIEWNAME", dialog.getViewName());
+        properties.setProperty("VIEWNAME", viewName);
         properties.setProperty("INITIALIZABLE", String.valueOf(dialog.isMakePresenterInitializableSelected()));
 
-        PsiDirectory directory = PsiManager.getInstance(e.getProject()).findDirectory(DataKeys.VIRTUAL_FILE.getData(e.getDataContext()));
-
-        FileTemplate viewTemplate = FileTemplateManager.getInstance(e.getProject()).getTemplate("PheruFXView");
-        FileTemplate presenterTemplate = FileTemplateManager.getInstance(e.getProject()).getTemplate("PheruFXPresenter");
-        FileTemplate fxmlTemplate = FileTemplateManager.getInstance(e.getProject()).getTemplate("PheruFXML");
-        FileTemplate cssTemplate = FileTemplateManager.getInstance(e.getProject()).getTemplate("PheruFXCss");
-        FileTemplate resourceBundleTemplate = FileTemplateManager.getInstance(e.getProject()).getTemplate("PheruFXResourceBundle");
-
-        WriteCommandAction.runWriteCommandAction(e.getProject(), () -> {
+        WriteCommandAction.runWriteCommandAction(event.getProject(), () -> {
             try {
-                PsiDirectory subdirectory = directory.createSubdirectory(dialog.getViewName().toLowerCase());
-                FileTemplateUtil.createFromTemplate(viewTemplate, dialog.getViewName() + "View", properties, subdirectory);
-                FileTemplateUtil.createFromTemplate(presenterTemplate, dialog.getViewName() + "Presenter", properties, subdirectory);
-                FileTemplateUtil.createFromTemplate(fxmlTemplate, dialog.getViewName().toLowerCase(), properties, subdirectory);
-                if (dialog.isCreateCssSelected()) {
-                    FileTemplateUtil.createFromTemplate(cssTemplate, dialog.getViewName().toLowerCase(), properties, subdirectory);
-                }
-                if(dialog.isCreateResourceBundleSelected()){
-                    FileTemplateUtil.createFromTemplate(resourceBundleTemplate, dialog.getViewName().toLowerCase(), properties, subdirectory);
-                }
-            } catch (Exception e1) {
-                throw new RuntimeException(e1);
+                PsiDirectory directory = createDirectories(sourceRootDirectory, qualifiedPackageName, viewName.toLowerCase());
+                PsiElement fileToSelect = createFiles(event.getProject(), directory, properties, viewName, dialog);
+                ProjectView.getInstance(event.getProject()).selectPsiElement(fileToSelect, false);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         });
     }
+
+    private String getQualifiedPackageForTargetDirectory(PsiDirectory sourceRootDirectory, PsiDirectory targetDirectory) {
+        if (sourceRootDirectory == targetDirectory) {
+            return "";
+        }
+        PsiDirectory currentDirectory = targetDirectory;
+        StringBuilder qualifiedPackageNameBuilder = new StringBuilder(currentDirectory.getName());
+        while (currentDirectory.getParentDirectory() != sourceRootDirectory) {
+            currentDirectory = currentDirectory.getParentDirectory();
+            qualifiedPackageNameBuilder.insert(0, ".");
+            qualifiedPackageNameBuilder.insert(0, currentDirectory.getName());
+        }
+        return qualifiedPackageNameBuilder.toString();
+    }
+
+    private PsiDirectory createDirectories(PsiDirectory sourceRootDirectory, String qualifiedPackageName, String viewPackageName) {
+        PsiDirectory currentDirectory = sourceRootDirectory;
+        for (String s : qualifiedPackageName.split("\\.")) {
+            if (!s.isEmpty()) {
+                PsiDirectory subdirectory = currentDirectory.findSubdirectory(s);
+                if (subdirectory == null) {
+                    subdirectory = currentDirectory.createSubdirectory(s);
+                }
+                currentDirectory = subdirectory;
+            }
+        }
+        currentDirectory = currentDirectory.createSubdirectory(viewPackageName);
+        return currentDirectory;
+    }
+
+    private PsiElement createFiles(Project project, PsiDirectory directory, Properties properties, String viewName,
+                                   CreateViewDialog dialog) throws Exception {
+        FileTemplate viewTemplate = FileTemplateManager.getInstance(project).getTemplate("PheruFXView");
+        FileTemplate presenterTemplate = FileTemplateManager.getInstance(project).getTemplate("PheruFXPresenter");
+        FileTemplate fxmlTemplate = FileTemplateManager.getInstance(project).getTemplate("PheruFXML");
+        FileTemplate cssTemplate = FileTemplateManager.getInstance(project).getTemplate("PheruFXCss");
+        FileTemplate resourceBundleTemplate = FileTemplateManager.getInstance(project).getTemplate("PheruFXResourceBundle");
+
+        if (dialog.isCreateResourceBundleSelected()) {
+            createFile(resourceBundleTemplate, viewName.toLowerCase(), properties, directory, dialog.isOpenResourceBundleInEditorSelected());
+        }
+        if (dialog.isCreateCssSelected()) {
+            createFile(cssTemplate, viewName.toLowerCase(), properties, directory, dialog.isOpenCSSInEditorSelected());
+        }
+        PsiElement view = createFile(viewTemplate, viewName + "View", properties, directory, dialog.isOpenViewInEditorSelected());
+        createFile(fxmlTemplate, viewName.toLowerCase(), properties, directory, dialog.isOpenFXMLInEditorSelected());
+        createFile(presenterTemplate, viewName + "Presenter", properties, directory, dialog.isOpenPresenterInEditorSelected());
+        return view;
+    }
+
+    private PsiElement createFile(FileTemplate template, String name, Properties properties, PsiDirectory directory, boolean openInEditor) throws Exception {
+        PsiElement file = FileTemplateUtil.createFromTemplate(template, name, properties, directory);
+        if (openInEditor) {
+            file.getContainingFile().navigate(true);
+        }
+        return file;
+    }
+
 }
--- src/de/pheru/fx/intellij/createview/CreateViewDialog.java
+++ src/de/pheru/fx/intellij/createview/CreateViewDialog.java
@@ -1,34 +1,67 @@
 package de.pheru.fx.intellij.createview;
 
-import com.intellij.openapi.compiler.Validator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.InputValidator;
-import com.intellij.openapi.ui.InputValidatorEx;
 import com.intellij.openapi.ui.ValidationInfo;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiNameHelper;
 import org.jetbrains.annotations.Nullable;
 
-import javax.help.UnsupportedOperationException;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateViewDialog extends DialogWrapper {
 
     private Project project;
 
-    private JTextField viewNameField;
-    private JCheckBox createCssBox;
-    private JCheckBox createResourceBundleBox;
-    private JCheckBox makePresenterInitializableBox;
+    private JTextField inputField = new JTextField();
+    private JCheckBox createCssBox = new JCheckBox("Create CSS");
+    private JCheckBox createResourceBundleBox = new JCheckBox("Create ResourceBundle");
+    private JCheckBox makePresenterInitializableBox = new JCheckBox("Make Presenter Initializable");
+    private JCheckBox openViewInEditor = new JCheckBox("View");
+    private JCheckBox openPresenterInEditor = new JCheckBox("Presenter", true);
+    private JCheckBox openFXMLInEditor = new JCheckBox("FXML", true);
+    private JCheckBox openCSSInEditor = new JCheckBox("CSS");
+    private JCheckBox openResourceBundleInEditor = new JCheckBox("ResourceBundle");
 
-    protected CreateViewDialog(Project project) {
+    private List<JCheckBox> openBoxes = createOpenBoxesList();
+
+    private boolean focusListenerInvoked = false;
+
+    private List<JCheckBox> createOpenBoxesList() {
+        List<JCheckBox> l = new ArrayList<>();
+        l.add(openCSSInEditor);
+        l.add(openFXMLInEditor);
+        l.add(openPresenterInEditor);
+        l.add(openResourceBundleInEditor);
+        l.add(openViewInEditor);
+        return l;
+    }
+
+    protected CreateViewDialog(Project project, String input) {
         super(project, true);
         this.project = project;
-        viewNameField = new JTextField();
-        createCssBox = new JCheckBox("Create CSS");
-        createResourceBundleBox = new JCheckBox("Create ResourceBundle");
-        makePresenterInitializableBox = new JCheckBox("Make Presenter Initializable");
+        inputField.setText(input);
+        inputField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (!focusListenerInvoked) {
+                    inputField.setSelectionStart(inputField.getText().length());
+                    inputField.setSelectionEnd(inputField.getText().length());
+                    focusListenerInvoked = true;
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                //nothing
+            }
+        });
         init();
         setTitle("PheruFXView");
     }
@@ -37,28 +70,79 @@ public class CreateViewDialog extends DialogWrapper {
     @Override
     protected JComponent createCenterPanel() {
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.add(viewNameField);
-        panel.add(createCssBox);
-        panel.add(createResourceBundleBox);
-        panel.add(new JSeparator(JSeparator.HORIZONTAL));
-        panel.add(makePresenterInitializableBox);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        addToPanelAlignLeft(panel, inputField);
+        addToPanelAlignLeft(panel, createCssBox);
+        addToPanelAlignLeft(panel, createResourceBundleBox);
+        addToPanelAlignLeft(panel, new JSeparator(JSeparator.HORIZONTAL));
+        addToPanelAlignLeft(panel, makePresenterInitializableBox);
+        addToPanelAlignLeft(panel, new JSeparator(JSeparator.HORIZONTAL));
+        addToPanelAlignLeft(panel, createOpenFilesPanel());
         return panel;
+    }
+
+    private JPanel createOpenFilesPanel() {
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        topPanel.add(new JLabel("Open in Editor"));
+        topPanel.add(Box.createRigidArea(new Dimension(40, 0)));
+        topPanel.add(createSelectUnselectAllLabel("Select all", true));
+        topPanel.add(Box.createRigidArea(new Dimension(25, 0)));
+        topPanel.add(createSelectUnselectAllLabel("Unselect all", false));
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        bottomPanel.add(openViewInEditor);
+        bottomPanel.add(openPresenterInEditor);
+        bottomPanel.add(openFXMLInEditor);
+        bottomPanel.add(openCSSInEditor);
+        bottomPanel.add(openResourceBundleInEditor);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        addToPanelAlignLeft(panel, topPanel);
+        addToPanelAlignLeft(panel, bottomPanel);
+        return panel;
+    }
+
+    private JLabel createSelectUnselectAllLabel(String text, boolean select) {
+        JLabel label = new JLabel(text);
+        label.setForeground(Color.BLUE);
+        label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                for (JCheckBox box : openBoxes) {
+                    box.setSelected(select);
+                }
+            }
+        });
+        return label;
+    }
+
+    private void addToPanelAlignLeft(JPanel panel, JComponent component) {
+        panel.add(component);
+        component.setAlignmentX(Component.LEFT_ALIGNMENT);
     }
 
     @Nullable
     @Override
     protected ValidationInfo doValidate() {
-        String viewName = viewNameField.getText();
-        if (viewName.isEmpty() || (viewName.length() > 0 && !PsiNameHelper.getInstance(project).isQualifiedName(viewName))) {
+        String input = inputField.getText();
+        if (input.isEmpty() || (input.length() > 0 && !PsiNameHelper.getInstance(project).isQualifiedName(input))) {
             return new ValidationInfo("This is not a valid Java qualified name!");
         }
         return null;
-
     }
 
-    public String getViewName() {
-        return viewNameField.getText();
+    @Nullable
+    @Override
+    public JComponent getPreferredFocusedComponent() {
+        return inputField;
+    }
+
+    public String getInput() {
+        return inputField.getText();
     }
 
     public boolean isCreateCssSelected() {
@@ -71,5 +155,25 @@ public class CreateViewDialog extends DialogWrapper {
 
     public boolean isMakePresenterInitializableSelected() {
         return makePresenterInitializableBox.isSelected();
+    }
+
+    public boolean isOpenViewInEditorSelected() {
+        return openViewInEditor.isSelected();
+    }
+
+    public boolean isOpenPresenterInEditorSelected() {
+        return openPresenterInEditor.isSelected();
+    }
+
+    public boolean isOpenFXMLInEditorSelected() {
+        return openFXMLInEditor.isSelected();
+    }
+
+    public boolean isOpenCSSInEditorSelected() {
+        return openCSSInEditor.isSelected();
+    }
+
+    public boolean isOpenResourceBundleInEditorSelected() {
+        return openResourceBundleInEditor.isSelected();
     }
 }
